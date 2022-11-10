66a3be2dda9eae3e34bd4ed32166829442a88aa1
Eric Jiang <ejiang@amazon.com>
2022-11-10 15:49:11 -0700

    wip

--- src/main/scala/me/ericjiang/boardgamelib/ai/AlphaBetaAgent.scala
+++ src/main/scala/me/ericjiang/boardgamelib/ai/AlphaBetaAgent.scala
@@ -7,10 +7,15 @@ import scala.Double.{NegativeInfinity => -∞, PositiveInfinity => ∞}
 class AlphaBetaAgent[S <: State[S]](depth: Int, heuristic: S => Double) extends Agent[S] {
 
   require(depth > 0)
+  require(heuristic != null)
 
   override def chooseAction(state: S): Action[S] =
-    state.availableActions
-      .maxBy(action => alphaBeta(action.execute(state).state, depth - 1, -∞, ∞, maximizingPlayer = false))
+    state.availableActions.maxBy(action => alphaBeta(
+      state = action.execute(state).state,
+      depth = depth - 1,
+      α = -∞,
+      β = ∞,
+      maximizingPlayer = false))
 
   protected def alphaBeta(state: S, depth: Int, α: Double, β: Double, maximizingPlayer: Boolean): Double = {
     val availableActions = state.availableActions
@@ -18,28 +23,20 @@ class AlphaBetaAgent[S <: State[S]](depth: Int, heuristic: S => Double) extends 
       heuristic(state)
     } else if (maximizingPlayer) {
       val (value, _) = availableActions.foldLeft((-∞, α)) {
-        case ((value, α), action) =>
-          if (α >= β) {
-            (value, α)
-          } else {
-            (
-              Math.max(value, alphaBeta(action.execute(state).state, depth - 1, α, β, maximizingPlayer = false)),
-              Math.max(α, value)
-            )
-          }
+        case ((value, α), _) if α >= β => (value, α)
+        case ((value, α), action) => (
+          Math.max(value, alphaBeta(action.execute(state).state, depth - 1, α, β, maximizingPlayer = false)),
+          Math.max(α, value)
+        )
       }
       value
     } else {
       val (value, _) = availableActions.foldLeft((∞, β)) {
-        case ((value, β), action) =>
-          if (β <= α) {
-            (value, β)
-          } else {
-            (
-              Math.min(value, alphaBeta(action.execute(state).state, depth - 1, α, β, maximizingPlayer = true)),
-              Math.min(β, value)
-            )
-          }
+        case ((value, β), _) if β <= α => (value, β)
+        case ((value, β), action) => (
+          Math.min(value, alphaBeta(action.execute(state).state, depth - 1, α, β, maximizingPlayer = true)),
+          Math.min(β, value)
+        )
       }
       value
     }
